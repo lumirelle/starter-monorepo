be9c4c4feab08c04816e9992d70595e6cdc68c62
Lumirelle <shabbyacc@outlook.com>
2025-09-07 22:00:43 +0800

    chore(tools): update czg & editor & taze config

---
+++ cz.config.js
@@ -0,0 +1,25 @@
+import { definePrompt } from 'czg'
+
+export default definePrompt({
+  markBreakingChangeMode: true,
+  allowBreakingChanges: ['feat', 'fix', 'chore'],
+  alias: {
+    typo: 'docs: fix typos',
+    readme: 'docs: update README.md',
+  },
+  scopeOverrides: {
+    feat: [
+      'pkg-placeholder',
+    ],
+    fix: [
+      'pkg-placeholder',
+    ],
+    refactor: [
+      'pkg-placeholder',
+    ],
+    chore: [
+      'deps',
+      'tools',
+    ],
+  },
+})

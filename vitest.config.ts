1feb00e50ab19edef62fc3598c3dd5925846edc5
Lumirelle <shabbyacc@outlook.com>
2025-12-06 00:22:56 +0800

    feat!: migrate to bun

--- vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { platform } from 'node:os'
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    testTimeout: platform() === 'win32' ? 10000 : 5000,
-  },
-})

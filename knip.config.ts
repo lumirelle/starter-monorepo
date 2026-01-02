abbf0a1509f9e52c0a94fe0e2481cd2f6bd4bc62
Lumirelle <shabbyacc@outlook.com>
2026-01-02 12:21:40 +0800

    chore: update project settings

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,32 @@
 import type { KnipConfig } from 'knip'
 
 export default {
-  // @keep-sorted
-  ignoreDependencies: [
-  ],
+  workspaces: {
+    '.': {
+      // @keep-sorted
+      ignoreDependencies: [
+        '@antfu/utils',
+        'bumpp',
+        'lint-staged',
+        'pkg-placeholder',
+      ],
+    },
+    'playground': {
+      // @keep-sorted
+      ignoreDependencies: [
+        'pkg-placeholder',
+      ],
+    },
+    'docs': {
+      // @keep-sorted
+      ignoreDependencies: [
+        '@iconify-json/svg-spinners',
+        '@unocss/reset',
+        '@vueuse/core',
+        'floating-vue',
+        'pinia',
+        'uno.css',
+      ],
+    },
+  },
 } satisfies KnipConfig

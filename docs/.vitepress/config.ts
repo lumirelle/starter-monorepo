9dfb409bcd658e82e7d8c3c551e7cba29897a5f7
Lumirelle <shabbyacc@outlook.com>
2026-01-14 15:54:05 +0800

    chore: update dependencies

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,9 +1,12 @@
 import type { DefaultTheme } from 'vitepress'
+import { fileURLToPath } from 'node:url'
 import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
+import UnoCSS from 'unocss/vite'
+import Components from 'unplugin-vue-components/vite'
+import Tsconfig from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitepress'
-import { groupIconMdPlugin } from 'vitepress-plugin-group-icons'
+import { groupIconMdPlugin, groupIconVitePlugin as GroupIconVitePlugin } from 'vitepress-plugin-group-icons'
 import { version } from '../../package.json'
-import vite from './vite.config'
 
 const GUIDES: DefaultTheme.NavItemWithLink[] & DefaultTheme.SidebarItem[] = [
   { text: 'Getting Started', link: '/guide/' },
@@ -19,6 +22,69 @@ const VERSIONS: (DefaultTheme.NavItemWithLink | DefaultTheme.NavItemChildren)[] 
 export default defineConfig({
   title: 'pkg-placeholder',
   description: '_description_',
+  head: [
+    // ['meta', { name: 'theme-color', content: '#ffffff' }],
+    // ['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' }],
+    ['meta', { name: 'author', content: 'Anthony Fu' }],
+    // ['meta', { property: 'og:title', content: '' }],
+    // ['meta', { property: 'og:image', content: '' }],
+    // ['meta', { property: 'og:description', content: '_description_' }],
+    // ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    // ['meta', { name: 'twitter:image', content: '' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, viewport-fit=cover' }],
+  ],
+
+  themeConfig: {
+    // logo: '/logo.svg',
+    nav: [
+      { text: 'Guide', items: [{ items: GUIDES }] },
+      { text: `v${version}`, items: VERSIONS },
+    ],
+    sidebar: {
+      '/': [
+        { text: 'Guide', items: GUIDES },
+      ],
+    },
+    editLink: {
+      pattern: 'https://github.com/antfu/pkg-placeholder/edit/main/docs/:path',
+      text: 'Suggest changes to this page',
+    },
+    search: {
+      provider: 'local',
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/antfu/pkg-placeholder' },
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2025-PRESENT Anthony Fu.',
+    },
+  },
+
+  cleanUrls: true,
+
+  vite: {
+    plugins: [
+      Tsconfig({
+        projects: [
+          fileURLToPath(new URL('../../tsconfig.json', import.meta.url)),
+        ],
+      }),
+      Components({
+        dirs: [
+          fileURLToPath(new URL('./components', import.meta.url)),
+        ],
+        dts: fileURLToPath(new URL('../components.d.ts', import.meta.url)),
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        extensions: ['vue', 'md'],
+      }),
+      UnoCSS(
+        fileURLToPath(new URL('./uno.config.ts', import.meta.url)),
+      ),
+      GroupIconVitePlugin(),
+    ],
+  },
+
   markdown: {
     theme: {
       light: 'vitesse-light',
@@ -32,59 +98,4 @@ export default defineConfig({
       md.use(groupIconMdPlugin)
     },
   },
-  cleanUrls: true,
-  vite,
-  themeConfig: {
-    // logo: '/logo.svg',
-    nav: [
-      {
-        text: 'Guide',
-        items: [
-          {
-            items: GUIDES,
-          },
-        ],
-      },
-      {
-        text: `v${version}`,
-        items: VERSIONS,
-      },
-    ],
-    sidebar: {
-      '/': [
-        {
-          text: 'Guide',
-          items: GUIDES,
-        },
-      ],
-    },
-    editLink: {
-      pattern: 'https://github.com/antfu/pkg-placeholder/edit/main/docs/:path',
-      text: 'Suggest changes to this page',
-    },
-    search: {
-      provider: 'local',
-    },
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/antfu/pkg-placeholder' },
-    ],
-
-    footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2025-PRESENT Anthony Fu.',
-    },
-  },
-
-  head: [
-    // ['meta', { name: 'theme-color', content: '#ffffff' }],
-    // ['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' }],
-    ['meta', { name: 'author', content: 'Anthony Fu' }],
-    // ['meta', { property: 'og:title', content: '' }],
-    // ['meta', { property: 'og:image', content: '' }],
-    // ['meta', { property: 'og:description', content: '_description_' }],
-    // ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
-    // ['meta', { name: 'twitter:image', content: '' }],
-    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, viewport-fit=cover' }],
-  ],
 })
